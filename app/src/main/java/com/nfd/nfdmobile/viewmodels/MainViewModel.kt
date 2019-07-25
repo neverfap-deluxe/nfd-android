eb60d7fe5ee859d7f751d6e610a34dc3819a4297
Julius Reade <julius.reade@fmclarity.com>
2019-07-25 14:37:34 +1000

    have my children

--- app/src/main/java/com/nfd/nfdmobile/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/nfd/nfdmobile/viewmodels/MainViewModel.kt
@@ -14,6 +14,9 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+// https://proandroiddev.com/oversimplified-network-call-using-retrofit-livedata-kotlin-coroutines-and-dsl-512d08eadc16
+
+
 class MainViewModel : ViewModel() {
 
   private val articles: MutableLiveData<List<NFDText>> by lazy {
